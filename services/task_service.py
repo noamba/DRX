eee3b890eff20310a60050fc9ac1125cee2eee76
noam <noamba@gmail.com>
2025-07-09 18:41:10 +0100

    doc

--- services/task_service.py
+++ services/task_service.py
@@ -14,12 +14,14 @@ Task = Query()
 class TaskService:
 
     def updates_tasks(self, tasks: list[PatientTask]):
+        """Updates the tasks in the database with the provided list of tasks."""
         # Question : This code is the result of a limitation by TinyDB. What is the issue and what feature
         # would a more complete DB solution offer ?
         for task in tasks:
             db.tasks.upsert(task.model_dump(), Task.id == task.id)
 
     def get_open_tasks(self) -> Iterable[PatientTask]:
+        """Returns an iterable of open PatientTask objects from the database."""
         return (
             PatientTask(**task_doc)
             for task_doc in db.tasks.search(where("status") == "Open")
