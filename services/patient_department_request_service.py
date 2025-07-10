727abe40ed4853c4062e6bf967890db171976ea6
noam <noamba@gmail.com>
2025-07-10 16:34:38 +0100

    handle requests per patient - dept AND remove tasks that were moved

--- services/patient_department_request_service.py
+++ services/patient_department_request_service.py
@@ -1,12 +1,8 @@
-from models.patient_task import PatientTask
-from .abstract_patient_request_service import PatientRequestService
 from typing import NewType
 
 from collections import defaultdict
 
-from uuid import uuid4
-
-from tinydb import where
+from tinydb import where, Query
 from models.patient_task import PatientTask
 from models.patient_request import PatientRequest
 from .abstract_patient_request_service import PatientRequestService
@@ -15,6 +11,9 @@ import db.db_tinydb as db
 from operator import attrgetter
 
 task_date_getter = attrgetter("updated_date")
+
+# Create a Query object for TinyDB queries
+query = Query()
 
 
 DepartmentToPatientTasks = NewType(
@@ -26,11 +25,14 @@ PatientToGroupedDepartmentTasks = dict[str, DepartmentToPatientTasks]
 
 class DepartmentPatientRequestService(PatientRequestService):
 
-
-    def get_open_patient_request(self, assigned_to) -> PatientRequest | None:
+    def get_open_patient_request(
+        self, patient_id, assigned_to
+    ) -> PatientRequest | None:
         """Retrieves from the DB the open patient request for a given patient_id"""
         result_dict = db.patient_requests.get(
-            (where("assigned_to") == assigned_to) & (where("status") == "Open")
+            (where("assigned_to") == assigned_to)
+            & (where("patient_id") == patient_id)
+            & (where("status") == "Open")
         )
 
         if not result_dict:
@@ -38,7 +40,50 @@ class DepartmentPatientRequestService(PatientRequestService):
 
         return PatientRequest(**result_dict)
 
+    def get_request_by_task(
+        self, task_id: str, exclude_request_id: str
+    ) -> PatientRequest | None:
+        """Retrieves from the DB a patient request for a given task_id,
+        excluding a specific request by its ID."""
+        result = db.patient_requests.search(
+            (query.task_ids.any(task_id)) & (query.id != exclude_request_id)
+        )
 
+        if not result:
+            return None
+
+        if len(result) > 1:
+            raise ValueError(
+                f"Multiple requests found with task_id {task_id}: {result}"
+            )
+
+        return PatientRequest(**result[0])
+
+    def remove_tasks_from_other_patient_requests(
+        self, task_ids: set, exclude_request_id: str
+    ):
+        """Assuming a task can appear only in one request, this method will:
+        1. Remove the tasks in task_ids from all other patient requests in the DB.
+        2. If a request has no tasks left, it will change it's status to `Closed`.
+        # TODO will need to look at messages and medications - do they need to be updated?
+        """
+        for task_id in task_ids:
+            request_by_task = self.get_request_by_task(
+                task_id=task_id,
+                exclude_request_id=exclude_request_id,
+            )
+
+            if request_by_task is not None:
+                request_by_task.task_ids.discard(task_id)
+                # if the request has no tasks left, close it
+                if not request_by_task.task_ids:
+                    request_by_task.status = "Closed"
+
+                # update the request in the DB
+                db.patient_requests.update(
+                    request_by_task.model_dump(),
+                    where("id") == request_by_task.id,
+                )
 
     def update_requests(self, tasks: list[PatientTask]):
         """Accepts a list of modified and open tasks and updates the relevant
@@ -46,22 +91,39 @@ class DepartmentPatientRequestService(PatientRequestService):
 
         # TODO: possible DRY with PerPatientRequestService
 
-        grouped_by_patent: dict[str, list[PatientTask]] = defaultdict(list)
+        # create a nested dictionary to group tasks by patient_id and department (assigned_to)
+        grouped_by_patient = defaultdict(lambda: defaultdict(list))
 
         for task in tasks:
-            grouped_by_patent[task.patient_id].append(task)
+            grouped_by_patient[task.patient_id][task.assigned_to].append(task)
 
-        for patient_id, patient_tasks in grouped_by_patent.items():
+        # iterate over the grouped tasks per patient and department
+        # and create/update patient requests
+        for patient_id, department_tasks in grouped_by_patient.items():
+            for assigned_to, patient_dept_tasks in department_tasks.items():
 
-            existing_req: PatientRequest = self.get_open_patient_request(patient_id)
-
-            pat_req = self.to_patient_request(patient_id, patient_tasks)
-
-            if not existing_req:
-                db.patient_requests.insert(pat_req.model_dump())
-            else:
-                pat_req.id = existing_req.id
-                db.patient_requests.update(
-                    pat_req.model_dump(), where("id") == existing_req.id
+                # get existing request for the patient and department
+                existing_req: PatientRequest = self.get_open_patient_request(
+                    assigned_to=assigned_to,
+                    patient_id=patient_id,
                 )
 
+                # create a new patient request object
+                pat_req = self.to_patient_request(patient_id, patient_dept_tasks)
+
+                # create OR update the request in the DB
+                if not existing_req:
+                    db.patient_requests.insert(pat_req.model_dump())
+                else:
+                    pat_req.id = existing_req.id
+                    db.patient_requests.update(
+                        pat_req.model_dump(), where("id") == existing_req.id
+                    )
+
+                # remove the tasks from *other* patient requests
+                # get relevant task ids
+                task_ids = {task.id for task in patient_dept_tasks}
+                self.remove_tasks_from_other_patient_requests(
+                    task_ids=task_ids,
+                    exclude_request_id=pat_req.id,
+                )
