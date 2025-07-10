ffd577a323af14b9afe6c93c931bee787b00df1d
noam <noamba@gmail.com>
2025-07-10 10:00:45 +0100

    Add DepartmentPatientRequestService get_open_patient_request

--- services/patient_department_request_service.py
+++ services/patient_department_request_service.py
@@ -2,6 +2,21 @@ from models.patient_task import PatientTask
 from .abstract_patient_request_service import PatientRequestService
 from typing import NewType
 
+from collections import defaultdict
+
+from uuid import uuid4
+
+from tinydb import where
+from models.patient_task import PatientTask
+from models.patient_request import PatientRequest
+from .abstract_patient_request_service import PatientRequestService
+import db.db_tinydb as db
+
+from operator import attrgetter
+
+task_date_getter = attrgetter("updated_date")
+
+
 DepartmentToPatientTasks = NewType(
     "DepartmentToPatientTasks", dict[str, list[PatientTask]]
 )
@@ -11,5 +26,14 @@ PatientToGroupedDepartmentTasks = dict[str, DepartmentToPatientTasks]
 
 class DepartmentPatientRequestService(PatientRequestService):
 
-    def update_requests(self, tasks: list[PatientTask]):
-        raise NotImplementedError
+
+    def get_open_patient_request(self, assigned_to) -> list(PatientRequest) | None:
+        """Retrieves from the DB the open patient request for a given patient_id"""
+        result_dict = db.patient_requests.get(
+            (where("assigned_to") == assigned_to) & (where("status") == "Open")
+        )
+
+        if not result_dict:
+            return None
+
+        return PatientRequest(**result_dict)
